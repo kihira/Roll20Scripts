72acc36728bf3b0300423a0602d964e4b693d109
Zoe Lee <zoe@kihira.uk>
2017-10-31 15:33:53 +0100

    SWRPG script now actually compiles \o/

--- SWRPG/SWRPG.ts
+++ SWRPG/SWRPG.ts
@@ -17,152 +17,118 @@ interface Obligations {
 
 interface State {
     swrpg: {
-        groups: string[];
-        activeColours: string[];
+        minionGroups: {
+            groups: string[][];
+            woundBar: string;
+            sizeBar: string;
+            colours: string[];
+            activeColours: string[];
+        }
     };
 }
 
 class SWRPG {
+    private chatName = "Dice System";
     private settings = {
-        command: /!swrpg/,
-        functions: [
-            {
-                func: swrpg.duty,
-                regex: /duty/
-            },
-            {
-                func: swrpg.obligation,
-                regex: /obligation/
-            },
-            {
-                func: swrpg.minion,
-                regex: /minion/
-            },
-            {
-                func: swrpg.chat,
-                regex: /chat/
-            }
-        ],
-        minion: {
-            woundBar: "bar1",
-            groupSizeBar: "bar3",
-            groupColours: ["red", "blue", "green", "brown", "purple", "pink", "yellow"]
-        },
-        name: "SWRPG",
-        chatname: "Dice System",
         gmsheetname: "-DicePool",
         debug: true
     };
 
-    private data = {
-        data: {
-            characters: [], // Array of player characters
-            gmsheet: ""
-        }
-    };
+    private characters: string[] = []; // Array of player characters
+    private gmsheet: string;
 
     public init() {
         const characters = findObjs({type: "character"});
         _.each(characters, (char) => {
             // We know that it would be in someones journal so can use that to reduce calls. Ignore 'all'
             if (char.get("controlledby").length > 4 && parseInt(getAttrByName(char.get("id"), "pcgm"), 10) === 1) {
-                this.data.characters.push(char.get("id"));
+                this.characters.push(char.get("id"));
             }
 
             if (char.get("name") === this.settings.gmsheetname) {
-                this.data.gmsheet = char.get("id");
-                this.logger("init", "GM Sheet: " + this.data.gmsheet, LogLevel.DEBUG);
+                this.gmsheet = char.get("id");
+                this.logger("init", "GM Sheet: " + this.gmsheet, LogLevel.DEBUG);
             }
         });
-        this.logger("init", `Built player cache of ${this.data.characters.length}: ${this.data.characters}`, LogLevel.INFO);
+        this.logger("init", `Built player cache of ${this.characters.length}: ${this.characters}`, LogLevel.INFO);
 
         // Create state property if it does not exist
         if (!state.swrpg) {
             state.swrpg = {
-                groups: [],
-                activeColours: []
+                minionGroups: {
+                    groups: [],
+                    woundBar: "bar1",
+                    sizeBar: "bar3",
+                    colours: ["red", "blue", "green", "brown", "purple", "pink", "yellow"],
+                    activeColours: []
+                },
             };
         }
     }
     public setupEventHandlers() {
-        on("change:graphic:" + this.settings.minion.woundBar + "_value", _.bind(this.handleChangeGraphic, this));
+        on("change:graphic:" + state.swrpg.minionGroups.woundBar + "_value", _.bind(this.handleChangeGraphic, this));
         on("chat:message", _.bind(this.handleInput, this));
     }
     private handleInput(msg: Message) {
         if (msg.type !== "api" || !msg.content.startsWith("!swrpg")) return;
 
-        for (let i = 0; i < this.settings.functions.length; i++) {
-            const key = this.settings.functions[i];
-            if (msg.content.match(key.regex)) {
-                // Find character player is speaking as
-                let speaking = null;
-                findObjs({_type: "character"}).forEach(function(chr) {
-                    if (chr.get("name") === msg.who) {
-                        speaking = chr;
-                    }
-                });
-
-                msg.content = msg.content.replace(this.settings.command, "");
-                msg.content = msg.content.replace(key.regex, "");
-                msg.content = msg.content.substr(2);
-
-                key.func(speaking, msg.content);
+        const tokenized = msg.content.split(" ");
+        const args = _.drop(tokenized, 2);
+        switch (tokenized[1]) {
+            case "duty":
+                sendChat(this.chatName, this.buildDutyObligationTable("Duty Check", "duty"));
                 break;
-            }
+            case "obligation":
+                sendChat(this.chatName, this.buildDutyObligationTable("Obligation Check", "obligation"));
+                break;
+            case "minion":
+                break;
         }
     }
     private handleChangeGraphic(obj: Roll20Object) {
         if (obj.get("_subtype") !== "token") return;
 
-        for (const i in state.swrpg.groups) {
-            const group = state.swrpg.groups[i];
-            // Minion is part of the group
-            if (group.indexOf(obj.get("_id")) !== -1) {
-                const wounds = parseInt(obj.get(this.settings.minion.woundBar + "_value"));
-                for (const tokenID in group) {
-                    if (!group.hasOwnProperty(tokenID)) continue;
+        for (const group of state.swrpg.minionGroups.groups) {
+            if (!_.contains(group, obj.id)) continue;
 
-                    const token = getObj(ObjTypes.Graphic, group[tokenID]);
-                    token.set(this.settings.minion.woundBar + "_value", wounds);
-                }
-                const count = Math.ceil(wounds / parseInt(getAttrByName(obj.get("represents"), "wounds", "max")));
-                if (count < group.length) {
-                    this.whisperGM("Remove " + (group.length - count) + " minion");
-                }
+            // Loop through and update wounds on all other tokens
+            const wounds = parseInt(obj.get(state.swrpg.minionGroups.woundBar + "_value"), 10);
+            for (const tokenID of group) {
+                const token = getObj(ObjTypes.Graphic, tokenID);
+                token.set(state.swrpg.minionGroups.woundBar + "_value", wounds);
             }
+            // Check if we're below threshold and if so, tell GM
+            const count = Math.ceil(wounds / parseInt(getAttrByName(obj.get("represents"), "wounds", "max"), 10));
+            if (count < group.length) {
+                this.whisperGM("Remove " + (group.length - count) + " minion");
+            }
+            break;
         }
     }
     private handleDestroyGraphic(obj: Roll20Object) {
         if (obj.get("_subtype") !== "token") return;
-        for (const i in state.swrpg.groups) {
-            let group = state.swrpg.groups[i];
-            // Check if this token is part of the group
-            if (group.indexOf(obj.get("_id")) !== -1) {
-                if (group.length === 1) { // If last minion, remove group and free colour
-                    const cols = obj.get("statusmarkers").split(",");
-                    let col = "";
-                    for (const c in this.settings.minion.groupColours) {
-                        col = this.settings.minion.groupColours[c];
-                        if (cols.indexOf(col) !== -1) {
-                            break;
-                        }
-                    }
-                    state.swrpg.activeColours.splice(state.swrpg.activeColours.indexOf(col), 1);
-                    state.swrpg.groups.splice(state.swrpg.groups.indexOf(group), 1);
-                    group = null;
-                }
-                else {
-                    group.splice(group.indexOf(obj.get("_id")), 1); // Remove from array
-                    for (const j in group) {
-                        if (!group.hasOwnProperty(j)) continue;
+        for (const group of state.swrpg.minionGroups.groups) {
+            if (!_.contains(group, obj.id)) continue;
 
-                        const tokenID = group[j];
-                        const token = getObj(ObjTypes.Graphic, tokenID);
-                        token.set(this.settings.minion.groupSizeBar + "_value", token.get(swrpg.settings.minion.groupSizeBar + "_value") - 1);
-                    }
+            if (group.length === 1) { // If last minion, remove group and free colour
+                const statusMarkers = obj.get("statusmarkers").split(",");
+                const marker = _.find(statusMarkers, (value) => {
+                    return _.contains(state.swrpg.minionGroups.colours, value);
+                });
+
+                if (marker !== undefined) {
+                    state.swrpg.minionGroups.activeColours = _.reject(state.swrpg.minionGroups.activeColours, (value) => value === marker);
+                    state.swrpg.minionGroups.groups.splice(state.swrpg.minionGroups.groups.indexOf(group), 1);
                 }
-                break;
             }
+            else {
+                group.splice(group.indexOf(obj.id), 1); // Remove from array
+                for (const tokenID of group) {
+                    const token = getObj(ObjTypes.Graphic, tokenID);
+                    token.set(state.swrpg.minionGroups.sizeBar + "_value", group.length);
+                }
+            }
+            break;
         }
     }
     private buildDutyObligationTable(title: string, pattern: string) {
@@ -172,7 +138,7 @@ class SWRPG {
         const typePattern = new RegExp("repeating_" + pattern + "_.+_" + pattern + "-type");
 
         // Build table
-        _.each(this.data.characters, (id: string) => {
+        _.each(this.characters, (id: string) => {
             const attrs = findObjs({type: "attribute", characterid: id});
             const name = getAttrByName(id, "name");
 
@@ -251,29 +217,37 @@ class SWRPG {
         return template;
     }
     private getColour() {
-        for (const col in this.settings.minion.groupColours) {
-            if (state.swrpg.activeColours.indexOf(col) === -1) {
-                state.swrpg.activeColours.push(col);
-                return this.settings.minion.groupColours[col];
+        for (const col in state.swrpg.minionGroups.colours) {
+            if (state.swrpg.minionGroups.activeColours.indexOf(col) === -1) {
+                state.swrpg.minionGroups.activeColours.push(col);
+                return state.swrpg.minionGroups.colours[col];
             }
         }
     }
+
     private whisperGM(message: string) {
-        sendChat(this.settings.chatname, "/w GM " + message, null, { noarchive: true });
+        sendChat(this.chatName, "/w GM " + message, null, { noarchive: true });
     }
-    private calcDamage(charID: string, message: string) {
-        const damage = /damage:(\d*)\|?/.exec(message)[1];
-        const pierce = /pierce:(\d*)\|?/.exec(message)[1];
+
+    private calcDamage(charID: string, message: string): string {
+        const damageRegex = /damage:(\d*)\|?/.exec(message);
+        const pierceRegex = /pierce:(\d*)\|?/.exec(message);
+
+        if (damageRegex == null || pierceRegex == null) return "";
+
+        const damage = parseInt(damageRegex[1], 10);
+        const pierce = parseInt(pierceRegex[1], 10);
         const parry = /parry/.exec(message) !== null;
-        const redSoak = Math.max(0, parseInt(getAttrByName(charID, "soak")) - pierce);
-        const valParry = 2 + parseInt(getAttrByName(charID, "talent-parry"));
+        const redSoak = Math.max(0, parseInt(getAttrByName(charID, "soak"), 10) - pierce);
+        const valParry = 2 + parseInt(getAttrByName(charID, "talent-parry"), 10);
 
         return "max(0,[[" + damage + "[Damage]-" + redSoak + "[Soak]" + (parry ? ("-" + valParry + "[Parry]") : "") + "+0d0]])";
     }
+
     private activate(tokenID: string) {
         const currToken = getObj(ObjTypes.Graphic, tokenID);
         const character = getObj(ObjTypes.Character, currToken.get("represents"));
-        const size = currToken.get(this.settings.minion.groupSizeBar + "_value");
+        const size = currToken.get(state.swrpg.minionGroups.sizeBar + "_value");
         const attribute = findObjs({
             type: "attribute",
             characterid: currToken.get("represents"),
@@ -282,16 +256,17 @@ class SWRPG {
         attribute.set("current", size);
         this.whisperGM("Minion group size to " + size + " for " + character.get("name"));
     }
+
     private createGroup(tokenID: string, count: number, createMinions: boolean) {
         const currToken = getObj(ObjTypes.Graphic, tokenID);
         const charID = currToken.get("represents");
         const wounds = parseInt(getAttrByName(charID, "npc-wounds", "max"), 10) * (count + 1);
 
         if (getAttrByName(charID, "npc-type") === "minion") {
-            currToken.set(this.settings.minion.groupSizeBar + "_value", count + 1);
-            currToken.set(this.settings.minion.groupSizeBar + "_max", count + 1);
-            currToken.set(this.settings.minion.woundBar + "_value", wounds);
-            currToken.set(this.settings.minion.woundBar + "_max", wounds);
+            currToken.set(state.swrpg.minionGroups.sizeBar + "_value", count + 1);
+            currToken.set(state.swrpg.minionGroups.sizeBar + "_max", count + 1);
+            currToken.set(state.swrpg.minionGroups.woundBar + "_value", wounds);
+            currToken.set(state.swrpg.minionGroups.woundBar + "_max", wounds);
             if (createMinions) {
                 const group = [ tokenID ];
                 const col = this.getColour();
@@ -312,50 +287,44 @@ class SWRPG {
                         layer: currToken.get("layer")
                     });
                     obj.set("status_" + col, true);
-                    obj.set(this.settings.minion.groupSizeBar + "_value", count + 1);
-                    obj.set(this.settings.minion.groupSizeBar + "_max", count + 1);
-                    obj.set(this.settings.minion.woundBar + "_value", wounds);
-                    obj.set(this.settings.minion.woundBar + "_max", wounds);
+                    obj.set(state.swrpg.minionGroups.sizeBar + "_value", count + 1);
+                    obj.set(state.swrpg.minionGroups.sizeBar + "_max", count + 1);
+                    obj.set(state.swrpg.minionGroups.woundBar + "_value", wounds);
+                    obj.set(state.swrpg.minionGroups.woundBar + "_max", wounds);
                     group.push(obj.get("_id"));
                 }
-                state.swrpg.groups.push(group);
+                state.swrpg.minionGroups.groups.push(group);
             }
         }
     }
-    private getGroup(tokenID: string) {
-        for (const i in state.swrpg.groups) {
-            const group = state.swrpg.groups[i];
-            // Minion is part of the group
-            if (group.indexOf(tokenID) !== -1) {
+
+    private getGroup(tokenID: string): string[] | false {
+        for (const group of state.swrpg.minionGroups.groups) {
+            if (_.contains(group, tokenID)) {
                 return group;
             }
         }
         return false;
     }
+
     private reset() {
-        state.swrpg.groups = [];
-        state.swrpg.activeColours = [];
+        state.swrpg.minionGroups.groups = [];
+        state.swrpg.minionGroups.activeColours = [];
         this.whisperGM("Minion groups reset");
     }
+
     private logger(functionName: string, msg: string, level: LogLevel) {
         if (level === undefined) level = LogLevel.DEBUG;
         if (level === LogLevel.INFO || (level === LogLevel.DEBUG && this.settings.debug)) {
-            log("(" + this.settings.name + ") " + functionName + ": " + msg);
+            log(`[SWRPG] ${functionName}: ${msg}`);
         }
     }
-    private duty() {
-        sendChat(this.settings.chatname, this.buildDutyObligationTable("Duty Check", "duty"));
-    }
-    private obligation() {
-        log("Obligation");
-        sendChat(this.settings.chatname, this.buildDutyObligationTable("Obligation Check", "obligation"));
-    }
+
     private chat(character: Roll20Object, message: string) {
         if (character === null) return;
 
-        let result;
-        result = /calcDamage\((.*?)\)/g.exec(message);
-        if (result) {
+        const result = /calcDamage\((.*?)\)/g.exec(message);
+        if (result != null) {
             message = message.replace(/calcDamage\((.*?)\)/g, this.calcDamage(character.get("id"), result[1]));
         }
         sendChat("character|" + character.get("id"), message);
